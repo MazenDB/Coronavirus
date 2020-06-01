03ab7e0db39b3f62b2323a199fb5c91e99219ed1
MazenDB <mazen.dibi@hotmail.com>
2020-06-02 00:12:51 +0400

    Update Coronavirus.sol

--- Coronavirus.sol
+++ Coronavirus.sol
@@ -1,7 +1,6 @@
-pragma solidity >=0.4.22 <0.7.0;
-contract Aggregator{
-    
-    
+pragma solidity =0.6.6;
+
+contract Registration{
     address public owner;
    
     struct oracle_type{
@@ -9,7 +8,7 @@ contract Aggregator{
         int infected;
         int recovered;
         int dead;
-        int credibility;
+        int reputation;
         uint lastUpdate;
    }
     
@@ -21,6 +20,80 @@ contract Aggregator{
     int public averagerecovered;
     int public averagedead; 
     
+    
+    event OracleRegistered(address oracleAddress);
+
+        modifier onlyOwner() { 
+        require(
+            msg.sender == owner,
+            "Only contract owner can call this."
+        );
+        _;
+    }
+   
+   constructor() public{
+       owner=msg.sender;
+   }
+    function registerOracle (address oracleAddress) public onlyOwner{
+       require(
+            !Oracles[oracleAddress].exists,
+            "Oracle already registred."
+        );
+       Oracles[oracleAddress].exists=true;
+       Oracles[oracleAddress].reputation=80;
+       oraclesArray.push(oracleAddress);
+       emit OracleRegistered(oracleAddress);
+   }
+
+}
+
+contract Reputation is Registration{
+    
+    int public trustworthiness_threshold;
+    
+   event ReputationUpdated(address oracleAddress, int reputationScore);
+
+    constructor() public{
+        trustworthiness_threshold = 90;
+
+    }
+    function computeReputation(address oracleAddress) public{
+        
+        int currentInfected = Oracles[oracleAddress].infected;
+        int cr;
+        int trustworthiness = 0;
+        int adjusting_factor = 4;
+        trustworthiness = averageinfected - currentInfected;
+        if(trustworthiness<0){
+            trustworthiness *= -1;
+        }
+        trustworthiness = 100 - trustworthiness;
+
+        if(trustworthiness>trustworthiness_threshold){
+            cr = (Oracles[oracleAddress].reputation*trustworthiness)/(4*adjusting_factor);
+            cr /= 100;
+            Oracles[oracleAddress].reputation += cr;
+        }
+        else{
+            cr = (Oracles[oracleAddress].reputation*trustworthiness)/(4*(10-adjusting_factor));
+            cr /= 100;
+            Oracles[oracleAddress].reputation -= cr;
+        }
+        if (Oracles[oracleAddress].reputation<0){
+            Oracles[oracleAddress].reputation = 0;
+        }
+        else if (Oracles[oracleAddress].reputation>100){
+            Oracles[oracleAddress].reputation = 100;
+        }
+        
+        emit ReputationUpdated(oracleAddress, Oracles[oracleAddress].reputation);
+    }
+    
+}
+
+contract Aggregator is Reputation{
+    
+
     int tolerance_thresold;
 
     struct head{
@@ -43,14 +116,7 @@ contract Aggregator{
 
     }
    
-    modifier onlyOwner() { 
-        require(
-            msg.sender == owner,
-            "Only contract owner can call this."
-        );
-        _;
-    }
-   
+
     modifier onlyOracle{ 
         require(
             Oracles[msg.sender].exists,
@@ -61,16 +127,8 @@ contract Aggregator{
     
    event OracleInput(address oracleAddress, int newInfected,int newRecovered, int newDead);
    
-   function registerOracle (address oracleAddress) public onlyOwner{
-       require(
-            !Oracles[oracleAddress].exists,
-            "Oracle already registred."
-        );
-       Oracles[oracleAddress].exists=true;
-       Oracles[oracleAddress].credibility=80;
-       oraclesArray.push(oracleAddress);
-   }
-   
+   event LatestStatistics(int Infected, int Recovered, int Dead);
+
    function inputOracle(int infect,int recover,int  death) public onlyOracle {
        
        Oracles[msg.sender].infected+=infect;
@@ -93,7 +151,7 @@ contract Aggregator{
         t.infectedCenter = t.value;
         t.reocveredCenter = Oracles[oraclesArray[0]].recovered;
         t.deadCenter = Oracles[oraclesArray[0]].dead;
-        t.cr = Oracles[oraclesArray[0]].credibility;
+        t.cr = Oracles[oraclesArray[0]].reputation;
         heads.push(t);
         for(uint i = 1;i<oraclesArray.length;i++){
             int min = 1000;
@@ -114,7 +172,7 @@ contract Aggregator{
                     }
                     if(min!=1000){
                         heads[min_index].votes++;
-                        heads[min_index].cr += Oracles[oraclesArray[i]].credibility;
+                        heads[min_index].cr += Oracles[oraclesArray[i]].reputation;
                         heads[min_index].infectedCenter += Oracles[oraclesArray[i]].infected;
                         heads[min_index].reocveredCenter += Oracles[oraclesArray[i]].recovered;
                         heads[min_index].deadCenter += Oracles[oraclesArray[i]].dead;
@@ -138,6 +196,8 @@ contract Aggregator{
                          averagedead = heads[j].deadCenter/heads[j].votes;
                      }
         }
+        
+        emit LatestStatistics(averageinfected,averagerecovered,averagedead);
     }
     
 }
